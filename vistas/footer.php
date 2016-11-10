250108e26604137593cb31047223681aa62ae515
Ramón Gago Carrera <ramongagocarrera@gmail.com>
2016-11-10 11:10:11 +0100

    footer

---
+++ vistas/footer.php
@@ -0,0 +1,15 @@
+<!--
+======================================================================
+Plantilla del footer y js necesarios en todas las paginas
+Creado por: Edgard Ruiz Gonzalez
+Fecha: 05/11/2016
+======================================================================
+-->
+<script src="../js/jquery.min.js"></script>
+<script src="../js/skel.min.js"></script>
+<script src="../js/skel-layers.min.js"></script>
+<script src="../js/initAdmin.js"></script>
+<script src="../js/Validaciones.js"></script>
+
+<!-- Bootstrap JavaScript -->
+<script src="../js/bootstrap.min.js"></script>
